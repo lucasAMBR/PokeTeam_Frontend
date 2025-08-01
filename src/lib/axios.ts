c399c571061dc77c8ce126b0ee38187278690a30
lucasAMBR <lucas.silvaambrosio_9119@outlook.com>
2025-08-01 12:03:08 -0300

    projeto base, axios configurado

---
+++ src/lib/axios.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import { getToken } from './token'
+
+export const api = axios.create({
+  baseURL: 'http://127.0.0.1:8000/api', // ajuste conforme sua API
+})
+
+// Adiciona token automaticamente no header
+api.interceptors.request.use((config) => {
+  const token = getToken()
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
